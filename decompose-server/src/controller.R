c1f8406ed28ec81f6fc6392b9fd27a2bfea7297f
Pavel Mekhnin <mekhninpv@gmail.com>
2023-06-18 14:56:22 +0300

    implement decomposition server

---
+++ decompose-server/src/controller.R
@@ -0,0 +1,9 @@
+#* @apiTitle decompose-server API
+
+#* @post /decompose
+function(req, width, height){
+    data <- matrix(jsonlite::fromJSON(req$postBody), nrow = as.numeric(height), ncol = as.numeric(width), byrow = TRUE)
+    s <- Rssa::ssa(data, svd.method = 'auto', kind = '2d-ssa', L = (dim(data) + 1) %/% 16)
+    gr <- Rssa::grouping.auto(s, grouping.method = 'wcor', method = 'average', nclust = 2)
+    attr(Rssa::reconstruct(s, groups = list(gr[[2]])), 'residuals')
+}
