b84f0141538d051d93b54da6b6e658584c46b17f
Pavel Mekhnin <mekhninpv@gmail.com>
2024-08-28 17:25:19 +0300

    refactoring

--- src/main/java/com/example/detection/service/DetectionService.java
+++
@@ -1,8 +0,0 @@
-package com.example.detection.service;
-
-import org.apache.commons.lang3.tuple.Pair;
-import org.opencv.core.Mat;
-
-public interface DetectionService {
-    Pair<Integer, Mat> analyze(byte[] payload);
-}
